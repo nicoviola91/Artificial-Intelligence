1c77502d65c5ac05cd4d62cd732a10d77219c72e
emiresenov <110808673+emiresenov@users.noreply.github.com>
2023-09-21 10:42:11 +0200

    Final

--- Assignment1/Assignment1.R
+++ Assignment1/Assignment1.R
@@ -83,7 +83,7 @@ aStar <- function(trafficMatrix, carInfo, packageMatrix) {
     path[[length(path)+1]] <- list(head[[1]], head[[2]]) 
     
     # Get neighbors of frontier node
-    neighbors <- getNeighbors(head[[1]], head[[2]], head[[3]], destX, destY, trafficMatrix)
+    neighbors <- getNeighbors(head[[1]], head[[2]], destX, destY, trafficMatrix)
     
     # Add unvisited neighbors to frontier
     for (n in neighbors) {
@@ -94,7 +94,7 @@ aStar <- function(trafficMatrix, carInfo, packageMatrix) {
         }
       }
       if (vis == FALSE) {
-        newNode <- list(n[[1]], n[[2]], n[[3]], path)
+        newNode <- list(n[[1]], n[[2]], head[[3]] + n[[3]], path)
         frontier[[length(frontier)+1]] <- newNode
         visited[[length(visited)+1]] <- newNode
       }
@@ -107,7 +107,7 @@ aStar <- function(trafficMatrix, carInfo, packageMatrix) {
 }
 
 # Function for getting neighbor nodes and their costs (f = g + h)
-getNeighbors <- function(x, y, cost, destX, destY, trafficMatrix) {
+getNeighbors <- function(x, y, destX, destY, trafficMatrix) {
   
   # Get traffic dimensions for checking bounds
   hEdges <- dim(trafficMatrix$hroads)[1]
@@ -118,22 +118,22 @@ getNeighbors <- function(x, y, cost, destX, destY, trafficMatrix) {
   
   # Right neighbor
   if (x <= hEdges) {
-    neighbors[[length(neighbors)+1]] <- list(x+1, y, cost + trafficMatrix$hroads[x, y] + abs(destX-(x+1)) + abs(destY-y))
+    neighbors[[length(neighbors)+1]] <- list(x+1, y, trafficMatrix$hroads[x, y] + abs(destX-(x+1)) + abs(destY-y))
   }
   
   # Left neighbor
   if (x-1 >= 1) {
-    neighbors[[length(neighbors)+1]] <- list(x-1, y, cost + trafficMatrix$hroads[x-1, y] + abs(destX-(x-1)) + abs(destY-y))
+    neighbors[[length(neighbors)+1]] <- list(x-1, y, trafficMatrix$hroads[x-1, y] + abs(destX-(x-1)) + abs(destY-y))
   }
   
   # Up neighbor
   if (y <= vEdges) {
-    neighbors[[length(neighbors)+1]] <- list(x, y+1, cost + trafficMatrix$vroads[x, y] + abs(destX-x) + abs(destY-(y+1)))
+    neighbors[[length(neighbors)+1]] <- list(x, y+1, trafficMatrix$vroads[x, y] + abs(destX-x) + abs(destY-(y+1)))
   }
   
   # Down neighbor
   if (y-1 >= 1) {
-    neighbors[[length(neighbors)+1]] <- list(x, y-1, cost + trafficMatrix$vroads[x, y-1] + abs(destX-x) + abs(destY-(y-1)))
+    neighbors[[length(neighbors)+1]] <- list(x, y-1, trafficMatrix$vroads[x, y-1] + abs(destX-x) + abs(destY-(y-1)))
   } 
   return(neighbors)
   
