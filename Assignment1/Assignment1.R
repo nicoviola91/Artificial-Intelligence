8040b8edf299721beefc5958d1778dfdd3be024e
emiresenov <110808673+emiresenov@users.noreply.github.com>
2023-09-21 10:38:42 +0200

    Update Assignment1.R

--- Assignment1/Assignment1.R
+++ Assignment1/Assignment1.R
@@ -139,6 +139,6 @@ getNeighbors <- function(x, y, cost, destX, destY, trafficMatrix) {
   
 }
 
-runDeliveryMan(carReady = myFunction, dim = 10, turns = 2000, doPlot = T, pause = 0.1, del = 5, verbose = T)
+#runDeliveryMan(carReady = myFunction, dim = 10, turns = 2000, doPlot = T, pause = 0.1, del = 5, verbose = T)
 
 testDM(myFunction, verbose = 0, returnVec = FALSE, n = 500, seed = 21, timeLimit = 250)
